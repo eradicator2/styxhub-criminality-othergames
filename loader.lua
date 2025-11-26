a6447dfa2625573855b7bcbf6f97bb9b7e36713b
eradicator2 <mvdstavropolck.ru@gmail.com>
2025-11-26 14:29:59 +0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,14 @@
+-- ts file was generated at discord.gg/25ms
+
+
+if game.PlaceId == 4588604953 or (game.PlaceId == 15169310267 or (game.PlaceId == 8343259840 or (game.PlaceId == 15169306359 or game.PlaceId == 15169303036))) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EarlFisherLoves/Charon/refs/heads/main/4588604953.lua"))()
+elseif game.PlaceId ~= 76455837887178 then
+    if game.PlaceId ~= 86430667919924 then
+        print("Game Not Supported!")
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EarlFisherLoves/Charon/refs/heads/main/86430667919924.lua"))()
+    end
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EarlFisherLoves/Charon/refs/heads/main/76455837887178.lua"))()
+end
